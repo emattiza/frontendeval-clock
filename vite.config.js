b1c1427f711f97dce3e9df41bbb67023fc172954
Evan Mattiza <emattiza@gmail.com>
2022-02-10 15:33:01 +0000

    configured for github pages build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import elmPlugin from "vite-plugin-elm";
 export default defineConfig({
   root: "public",
   server: { hmr: { clientPort: 443 } },
+  base: "/frontendeval-clock/",
   plugins: [elmPlugin()],
 });
